e02d7f1f50922d187d3afb8af4888013501819bc
jorgepeniaranda <jorge.maxii25@gmail.com>
2023-09-14 13:49:58 -0300

    add emailjs, form is working

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -1,12 +1,32 @@
 "use client";
 
 import React from "react";
+import emailjs from "@emailjs/browser";
 
 export const ContactForm = ({ className }: { className?: string }) => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_v055els",
+        "template_jlv215d",
+        e.currentTarget,
+        "5N89hdd9Q-Tpjh0fF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <form onSubmit={() => event?.preventDefault()} className={className}>
-      <input type="text" placeholder="Nombre..." required />
-      <input type="email" placeholder="Email..." required />
+    <form onSubmit={sendEmail} className={className}>
+      <input type="text" name="name" placeholder="Nombre..." required />
+      <input type="email" name="email" placeholder="Email..." required />
       <textarea
         name="message"
         cols={30}
