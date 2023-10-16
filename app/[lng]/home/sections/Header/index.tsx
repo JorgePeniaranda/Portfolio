33b1b31c4d398ae789a99dc41703c9741641187c
JorgePeniaranda <peniarandajorge@gmail.com>
2023-10-15 22:02:05 -0300

    fix: implement the correct use of h1

--- app/[lng]/home/sections/Header/index.tsx
+++ app/[lng]/home/sections/Header/index.tsx
@@ -9,17 +9,17 @@ export default async function Header({ lng }: { lng: string }) {
     <header className="w-full min-h-[calc(100vh_-_9rem)] flex flex-wrap justify-around select-none gap-16 sm:gap-0">
       {/* h-[calc(100%_-_7.5rem)]: 100% - nav size */}
       <div className="flex flex-col justify-evenly text-center md:text-start w-[90%] md:w-[70%]">
-        <h1 className="font-bold text-5xl sm:text-7xl">
+        <p className="font-bold text-5xl sm:text-7xl">
           {t("header-title")}{" "}
           <strong
             className={"font-bold text-5xl sm:text-7xl " + styles.specialWord}
           >
             {t("header-title-special")}
           </strong>
-        </h1>
-        <span className="text-zinc-400  font-semibold text-xl select-text">
+        </p>
+        <h1 className="text-zinc-400  font-semibold text-xl select-text">
           Jorge Peñaranda / {t("category")}
-        </span>
+        </h1>
       </div>
       <figure className="flex items-center justify-center pb-32 w-[90%] sm:w-[30%]">
         <Rocket
