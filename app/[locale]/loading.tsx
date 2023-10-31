732a2e8dac42e57d4d0721238ccf15b22da68f11
JorgePeniaranda <peniarandajorge@gmail.com>
2023-10-30 22:44:43 -0300

    fix loader

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,7 +1,9 @@
+import styles from '@/styles/loading.module.css'
+
 export default function Loading () {
   return (
-    <div className='bg-] grid place-items-center w-full h-full fixed top-0 left-0 '>
-      asdsadasd
+    <div className='bg-black grid place-items-center w-full h-full fixed top-0 left-0  z-50'>
+      <span className={styles.loader} />
     </div>
   )
 }
