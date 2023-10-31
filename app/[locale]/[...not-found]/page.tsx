9868e1b9670d1b878e582f6d4238490843261d2a
JorgePeniaranda <peniarandajorge@gmail.com>
2023-10-30 22:40:49 -0300

    refactor: update notfound svg

--- app/[locale]/[...not-found]/page.tsx
+++ app/[locale]/[...not-found]/page.tsx
@@ -6,9 +6,9 @@ export default function Loading () {
   const t = useTranslations('NotFound')
 
   return (
-    <Link href='/' className='flex flex-col items-center justify-evenly mx-auto w-fit min-h-[calc(100vh_-_9rem)] hover:scale-110 transition-all ease-linear'>
-      <NotFound />
-      <p className='text-3xl font-bold'>{t('404')} 😔</p>
+    <Link href='/' className='flex flex-col items-center justify-evenly mx-auto w-fit h-[calc(100vh_-_9rem)] hover:scale-110 transition-all ease-linear'>
+      <NotFound className='h-3/4' />
+      <p className='text-3xl font-bold'>{t('404')}</p>
     </Link>
   )
 }
