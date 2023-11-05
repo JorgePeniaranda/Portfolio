39b2c14ba8514c893aeed1dff900504f3330b9b1
JorgePeniaranda <peniarandajorge@gmail.com>
2023-11-04 21:24:24 -0300

    Implement dynamic blog site

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Jorge Peñaranda | Blog";
+export const SITE_DESCRIPTION =
+  "Sumérgete en el mundo del desarrollo web y el aprendizaje constante. Exploramos HTML, JavaScript, CSS, frameworks de la actualidad y mucho más.";
