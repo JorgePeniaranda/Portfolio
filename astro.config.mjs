9d673dafe220d463c73b3e0c019f2d2830ced489
JorgePeniaranda <peniarandajorge@gmail.com>
2023-11-12 15:53:39 -0300

    feat: add react + feat: add popover for choosing current theme color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jorgepeniaranda.me",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), react()]
 });
