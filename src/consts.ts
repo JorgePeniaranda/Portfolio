b29c367e7987114f59b7545d445b7a0ed1acecb8
JorgePeniaranda <peniarandajorge@gmail.com>
2024-05-21 11:20:42 -0300

    refactor: remove files

--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Jorge Peñaranda | Blog";
-export const SITE_DESCRIPTION =
-  "Sumérgete en el mundo del desarrollo web y el aprendizaje constante. Exploramos HTML, JavaScript, CSS, frameworks de la actualidad y mucho más.";
