fd6398cd4d108d17c0e57fe7b4ef4b2764007781
JorgePeniaranda <peniarandajorge@gmail.com>
2024-06-05 12:33:21 -0300

    feat: install utils astro integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { ORIGINAL_URL } from "./src/constants/seo";
+import playformCompress from "@playform/compress";
+import icon from "astro-icon";
 
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
   site: ORIGINAL_URL,
-  integrations: [tailwind(), sitemap(), react()]
+  integrations: [tailwind(), sitemap(), react(), playformCompress(), icon(), robotsTxt()]
 });
