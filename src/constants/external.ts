af3299ef83dae215952300d58c5206d7f8faa62a
JorgePeniaranda <peniarandajorge@gmail.com>
2024-06-20 16:51:10 -0300

    feat: create constants with external info

---
+++ src/constants/external.ts
@@ -0,0 +1,33 @@
+//#region Personal Accounts
+export const PERSONAL_ACCOUNTS = {
+  github: {
+    url: 'https://github.com/JorgePeniaranda',
+    username: 'JorgePeniaranda',
+    image: 'https://avatars.githubusercontent.com/u/80211041?v=4'
+  },
+  gitlab: {
+    url: 'https://gitlab.com/JorgePeniaranda',
+    username: 'JorgePeniaranda'
+  },
+  codewars: {
+    url: 'https://www.codewars.com/users/JorgePeniaranda',
+    username: 'JorgePeniaranda'
+  },
+  linkedin: {
+    url: 'https://www.linkedin.com/in/jorgepeniaranda/',
+    username: 'JorgePeniaranda'
+  },
+  twitter: {
+    url: 'https://twitter.com/PeniarandaJorge',
+    username: 'PeniarandaJorge'
+  },
+  instagram: {
+    url: 'https://www.instagram.com/jorgepeniaranda/',
+    username: 'JorgePeniaranda'
+  }
+} as const
+//#endregion
+
+//#region API URLs
+export const GITHUB_GET_USERS_URL = 'https://api.github.com/users'
+//#endregion
