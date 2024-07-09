7868f9a68b4967d87dc4e19a4cf799a9a38bbf7a
jorgepeniaranda <peniarandajorge@gmail.com>
2024-07-08 23:19:17 -0300

    chore: Add playformCompress integration and enable HTML compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// import playformCompress from "@playform/compress";
+import playformCompress from "@playform/compress";
 import icon from "astro-icon";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jorgepeniaranda.dev.ar",
-  integrations: [tailwind(), sitemap(), react(), icon(), robotsTxt()]
+  integrations: [tailwind(), sitemap(), react(), playformCompress(), icon(), robotsTxt()],
+  compressHTML: true,
 });
