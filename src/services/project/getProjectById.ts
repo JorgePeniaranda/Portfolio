31ee347d8d0947c6c4ab252fa293ecc063382dfe
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-27 18:30:02 -0300

    feat: remove unused API routes and types related to projects

--- src/services/project/getProjectById.ts
+++ src/services/project/getProjectById.ts
@@ -3,9 +3,13 @@ import type {Project} from "@prisma/client";
 import {prisma} from "../../helpers/client/prisma-client";
 
 export async function getProjectById({id}: {id: Project["id"]}): Promise<Project | null> {
-  return await prisma.project.findUnique({
+  const data = await prisma.project.findUnique({
     where: {
       id,
     },
   });
+
+  console.log({data});
+
+  return data;
 }
