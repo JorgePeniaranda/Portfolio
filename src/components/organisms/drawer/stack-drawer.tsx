7dd92dee6b90976b15f6f8a974579fedb7bb0c5d
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-29 16:25:53 -0300

    feat: enhance StackDrawer component with optional props for drawer control and navigation

--- src/components/organisms/drawer/stack-drawer.tsx
+++ src/components/organisms/drawer/stack-drawer.tsx
@@ -2,28 +2,64 @@ import type {Project, Stack} from "@prisma/client";
 
 import * as React from "react";
 import {X} from "lucide-react";
+import {Drawer as DrawerPrimitive} from "vaul";
 
 import {Drawer, DrawerClose, DrawerContent, DrawerTrigger} from "../../../components/ui/drawer";
-import {isDefined} from "../../../helpers/guards/is-defined";
+import {isDefined, isNotDefined} from "../../../helpers/guards/is-defined";
 
 /**
  * Component that renders an interactive drawer to display detailed information about a Stack.
+ * The drawer can be triggered by the `triggerChild` and displays stack data along with related projects.
+ * It also allows navigation to a specific site and preserves state or navigates back based on the provided `returnToSiteOnClose` property.
  *
  * @param {Object} props - Component properties.
- * @param {React.ReactNode} props.triggerChild - React node acting as a trigger to open the drawer.
- * @param {Stack & {projects: Pick<Project, "id" | "key" | "name" | "logoUrl">[]}} props.stack - Stack data to be displayed in the drawer.
+ * @param {React.ReactNode} [props.triggerChild] - Optional React node acting as a trigger to open the drawer.
+ * @param {React.ComponentProps<typeof DrawerPrimitive.Root>} [props.drawerProps] - Optional props for controlling the DrawerPrimitive component, such as open state, close behavior, etc.
+ * @param {Object} [props.returnToSiteOnClose] - Optional configuration for URL navigation when the drawer is closed.
+ * @param {string} props.returnToSiteOnClose.site - The URL to navigate to when the drawer is closed.
+ * @param {boolean} props.returnToSiteOnClose.keepState - Flag to indicate if the application state should be preserved when navigating to `site`.
+ * @param {Stack & {projects: Pick<Project, "id" | "key" | "name" | "logoUrl">[]}} props.stack - Stack data to be displayed in the drawer, including associated projects.
  */
 export function StackDrawer({
   triggerChild,
   stack,
+  drawerProps,
+  returnToSiteOnClose,
 }: {
-  triggerChild: React.ReactNode;
+  triggerChild?: React.ReactNode;
+  drawerProps?: React.ComponentProps<typeof DrawerPrimitive.Root>;
+  returnToSiteOnClose?: {
+    site: string;
+    keepState: boolean;
+  };
   stack: Stack & {
     projects: Pick<Project, "id" | "key" | "name" | "logoUrl">[];
   };
 }) {
+  const handleDrawerClose = () => {
+    if (isDefined(returnToSiteOnClose)) {
+      if (returnToSiteOnClose.keepState) {
+        history.pushState(null, "", returnToSiteOnClose.site);
+
+        return;
+      }
+
+      window.location.href = returnToSiteOnClose.site;
+    }
+
+    // If drawerProps is not defined, exit the function
+    if (isNotDefined(drawerProps)) {
+      return;
+    }
+
+    // If onClose is defined in drawerProps, call it to close the drawer
+    if (isDefined(drawerProps.onClose)) {
+      drawerProps.onClose();
+    }
+  };
+
   return (
-    <Drawer>
+    <Drawer {...drawerProps} onClose={handleDrawerClose}>
       <DrawerTrigger asChild>{triggerChild}</DrawerTrigger>
       <DrawerContent className="overflow-y-auto overflow-x-hidden px-6 py-2">
         <section className="space-y-4">
