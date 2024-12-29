023d05042a9929a7b267527940d222de09c01193
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-29 16:23:28 -0300

    fix: update comments and improve prop type descriptions in StackDrawer component

--- src/components/organisms/drawer/stack-drawer.tsx
+++ src/components/organisms/drawer/stack-drawer.tsx
@@ -7,19 +7,11 @@ import {Drawer, DrawerClose, DrawerContent, DrawerTrigger} from "../../../compon
 import {isDefined} from "../../../helpers/guards/is-defined";
 
 /**
- * Componente que renderiza un Drawer interactivo para mostrar información detallada de un Stack.
+ * Component that renders an interactive drawer to display detailed information about a Stack.
  *
- * @param {Object} props - Propiedades del componente.
- * @param {React.ReactNode} props.triggerChild - Nodo React que actúa como disparador para abrir el Drawer.
- * @param {Object} props.stack - Datos del Stack que se mostrarán en el Drawer.
- * @param {string} props.stack.name - Nombre del stack.
- * @param {string} props.stack.iconUrl - URL del ícono del stack.
- * @param {string} [props.stack.description] - Descripción opcional del stack.
- * @param {Array} props.stack.projects - Lista de proyectos relacionados con el stack.
- * @param {string} props.stack.projects[].id - Identificador único del proyecto.
- * @param {string} props.stack.projects[].key - Clave única del proyecto.
- * @param {string} props.stack.projects[].name - Nombre del proyecto.
- * @param {string} props.stack.projects[].logoUrl - URL del logo del proyecto.
+ * @param {Object} props - Component properties.
+ * @param {React.ReactNode} props.triggerChild - React node acting as a trigger to open the drawer.
+ * @param {Stack & {projects: Pick<Project, "id" | "key" | "name" | "logoUrl">[]}} props.stack - Stack data to be displayed in the drawer.
  */
 export function StackDrawer({
   triggerChild,
@@ -35,12 +27,12 @@ export function StackDrawer({
       <DrawerTrigger asChild>{triggerChild}</DrawerTrigger>
       <DrawerContent className="overflow-y-auto overflow-x-hidden px-6 py-2">
         <section className="space-y-4">
-          {/* Header con el logo y el nombre del stack */}
+          {/* Header with the stack logo and name */}
           <div className="relative flex items-center space-x-2">
             <img
               alt={`${stack.name} stack logo`}
               className="aspect-square size-10 rounded-sm"
-              src={`${stack.iconUrl} logo`}
+              src={stack.iconUrl}
             />
             <h3 className="text-4xl font-bold">{stack.name}</h3>
             <DrawerClose className="absolute right-0 top-0 p-2">
@@ -48,7 +40,7 @@ export function StackDrawer({
             </DrawerClose>
           </div>
 
-          {/* Descripción del stack (si está definida) */}
+          {/* Stack description (if defined) */}
           {isDefined(stack.description) && (
             <article>
               <h4 className="font-bold underline underline-offset-2">Descripción:</h4>
@@ -56,7 +48,7 @@ export function StackDrawer({
             </article>
           )}
 
-          {/* Proyectos relacionados (si existen) */}
+          {/* Related projects (if any) */}
           {stack.projects.length > 0 && (
             <article className="space-y-1">
               <h4 className="font-bold underline underline-offset-2">Proyectos relacionados:</h4>
@@ -64,7 +56,7 @@ export function StackDrawer({
                 {stack.projects.map((project) => (
                   <li key={project.id}>
                     <a
-                      className="flex items-center space-x-1 transition-all ease-linear hover:translate-x-2"
+                      className="flex items-center transition-all ease-linear hover:translate-x-2"
                       href={`/projects/${project.key}`}
                       rel="noreferrer"
                       target="_blank"
@@ -72,7 +64,7 @@ export function StackDrawer({
                       <img
                         alt={`${project.name} logo`}
                         className="aspect-square size-14 rounded-sm"
-                        src={`${project.logoUrl} logo`}
+                        src={project.logoUrl}
                       />
                       <h5 className="whitespace-nowrap text-xl">{project.name}</h5>
                     </a>
