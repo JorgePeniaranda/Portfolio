ad44d04181ce6591094eb289cf7b2ed330eb58ca
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-31 12:19:39 -0300

    feat: add service functions to retrieve collaborators and projects with minimal data for improved performance

---
+++ src/services/stack/getStackWithRelationsAndProjectsMinById.ts
@@ -0,0 +1,42 @@
+import type {Project, Stack} from "@prisma/client";
+
+import {databaseClient} from "../../helpers/client/prisma";
+
+export async function getStackWithRelationsAndProjectsMinByKey({key}: {key: Stack["key"]}): Promise<
+  | (Stack & {
+      associatedProjects: Pick<Project, "id" | "key" | "name" | "logoUrl">[];
+      relatedFrom: {
+        toStackStack: Stack;
+      }[];
+      relatedTo: {
+        fromStackStack: Stack;
+      }[];
+    })
+  | null
+> {
+  return await databaseClient.stack.findUnique({
+    where: {
+      key,
+    },
+    include: {
+      associatedProjects: {
+        select: {
+          id: true,
+          key: true,
+          name: true,
+          logoUrl: true,
+        },
+      },
+      relatedFrom: {
+        include: {
+          toStackStack: true, // Obtener los stacks destino relacionados desde `relatedFrom`
+        },
+      },
+      relatedTo: {
+        include: {
+          fromStackStack: true, // Obtener los stacks origen relacionados desde `relatedTo`
+        },
+      },
+    },
+  });
+}
