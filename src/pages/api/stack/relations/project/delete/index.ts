8f4c6cd466b8700834442a0806664089d2cacf54
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-31 10:22:38 -0300

    feat: add API endpoints to manage project relationships for collaborators and stacks

---
+++ src/pages/api/stack/relations/project/delete/index.ts
@@ -0,0 +1,40 @@
+import type {APIRoute} from "astro";
+
+import {databaseClient} from "../../../../../../helpers/client/prisma";
+import {RequestHandler} from "../../../../../../helpers/common/request-handler";
+import {RelationshipsSchema} from "../../../../../../schemas/common/relationships";
+
+// Disable prerendering for this route
+export const prerender = false;
+
+/**
+ * PATCH handler to remove a relation between a stack and a project.
+ * - Parses and validates the request body.
+ * - Disconnects the specified project from the stack in the database.
+ */
+export const PATCH: APIRoute = ({request}) => {
+  return RequestHandler(
+    async () => {
+      const body = await request.json();
+      const validationResult = RelationshipsSchema.parse(body);
+
+      const response = await databaseClient.stack.update({
+        data: {
+          projects: {
+            disconnect: {
+              id: validationResult.idTo,
+            },
+          },
+        },
+        where: {id: validationResult.idFrom},
+      });
+
+      return {
+        success: true,
+        message: "Relation deleted successfully",
+        data: response,
+      };
+    },
+    {successStatusCode: 200},
+  );
+};
