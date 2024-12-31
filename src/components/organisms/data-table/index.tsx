439f7ad2f9c2fe5b895115a6bb5935ed6df01234
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-31 09:33:59 -0300

    feat: update page size options in DataTable component

--- src/components/organisms/data-table/index.tsx
+++ src/components/organisms/data-table/index.tsx
@@ -93,7 +93,7 @@ export function DataTable<TData, TValue>({
                   <SelectValue placeholder={table.getState().pagination.pageSize} />
                 </SelectTrigger>
                 <SelectContent side="top">
-                  {[10, 20, 30, 40, 50].map((pageSize) => (
+                  {[10, 15, 20, 30, 40, 50].map((pageSize) => (
                     <SelectItem key={pageSize} value={`${pageSize}`}>
                       {pageSize}
                     </SelectItem>
