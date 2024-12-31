7d7c0de789e8049884a83e25561e2dbb62788b14
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-31 12:20:37 -0300

    feat: enhance environment configuration by adding development check and server-side enable flag

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -4,14 +4,18 @@ import {getEnvValue} from "../helpers/common/get-env";
 // Check if the environment is production based on the `import.meta.env.PROD` value
 const isProduction = import.meta.env.PROD;
 
+// Check if the environment is development based on the `import.meta.env.DEV` value
+const isDevelopment = import.meta.env.DEV;
+
 /**
  * Configuration object for environment-specific values.
  * @constant {object}
  */
 export const ENV = {
   isProduction,
-  isDevelopment: import.meta.env.DEV,
+  isDevelopment,
   base_url: isProduction ? getEnvValue("SITE") : "http://localhost:4321",
   secret_code: getEnvValue("PUBLIC_SECRET_CODE", "7421"),
   appVersion: packageJson.version,
+  isServerSideEnable: isDevelopment,
 } as const;
