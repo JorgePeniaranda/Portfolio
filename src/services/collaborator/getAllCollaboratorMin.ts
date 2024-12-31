1c9bdf7e624c61c00583f95ddb8596bea2fc7da1
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-30 21:08:53 -0300

    feat: include githubUsername in getAllCollaboratorMin service response

--- src/services/collaborator/getAllCollaboratorMin.ts
+++ src/services/collaborator/getAllCollaboratorMin.ts
@@ -2,11 +2,14 @@ import type {Collaborator} from "@prisma/client";
 
 import {databaseClient} from "../../helpers/client/prisma";
 
-export async function getAllCollaboratorMin(): Promise<Pick<Collaborator, "id" | "nickname">[]> {
+export async function getAllCollaboratorMin(): Promise<
+  Pick<Collaborator, "id" | "nickname" | "githubUsername">[]
+> {
   return await databaseClient.collaborator.findMany({
     select: {
       id: true,
       nickname: true,
+      githubUsername: true,
     },
   });
 }
