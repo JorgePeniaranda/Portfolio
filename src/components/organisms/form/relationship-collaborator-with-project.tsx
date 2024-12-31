2ca17acc65e06983c190bef9b58d3caedc669f58
jorgepeniaranda <peniarandajorge@gmail.com>
2024-12-31 10:11:43 -0300

    feat: add RelationshipCollaboratorWithProject component and integrate with UpdateCollaboratorForm for project management

---
+++ src/components/organisms/form/relationship-collaborator-with-project.tsx
@@ -0,0 +1,140 @@
+import type {Project, Stack} from "@prisma/client";
+import type {RelationshipsSchema} from "../../../schemas/common/relationships";
+
+import {Plus} from "lucide-react";
+import {useForm} from "react-hook-form";
+
+import {useToast} from "../../../hooks/use-toast";
+import {patchAddRelationWithCollaborator} from "../../../services/project/patchAddRelationWithCollaborator";
+import {Button} from "../../ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../ui/dialog";
+import {Form, FormControl, FormField, FormItem} from "../../ui/form";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "../../ui/select";
+
+export function RelationshipCollaboratorWithProject({
+  idFrom,
+  availableProject,
+  disableForm = false,
+}: {
+  idFrom: Stack["id"];
+  availableProject: Pick<Project, "id" | "name" | "logoUrl">[];
+  disableForm?: boolean;
+}) {
+  const {toast} = useToast();
+  const form = useForm<RelationshipsSchema>({
+    defaultValues: {
+      idFrom,
+    },
+  });
+
+  const onSubmit = async (values: RelationshipsSchema) => {
+    console.log({values});
+    const response = await patchAddRelationWithCollaborator({
+      idFrom: Number(values.idTo),
+      idTo: Number(values.idFrom),
+    });
+
+    if (response.success) {
+      form.reset();
+      toast({
+        title: "Proyecto relacionado con el colaborador",
+        description: response.message,
+        className: "bg-green-500",
+      });
+    }
+
+    if (!response.success) {
+      toast({
+        title: "Error al relacionar proyecto con el colaborador",
+        description: response.message,
+        className: "bg-red-500",
+      });
+    }
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button
+          aria-label="Relacionar stack"
+          className="my-5 flex aspect-square w-max flex-col items-center justify-center rounded-lg bg-zinc-300 p-6 shadow dark:bg-zinc-800"
+          type="button"
+        >
+          <Plus className="size-20" />
+        </button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-center">Relacionar proyecto</DialogTitle>
+          <DialogDescription>
+            Selecciona un proyecto para relacionar con el colaborador.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form
+            className="space-y-8"
+            id="relation-with-project-from-collaborator"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-wrap gap-2">
+              <FormField
+                control={form.control}
+                name="idTo"
+                render={({field}) => (
+                  <FormItem className="mx-auto">
+                    <FormControl>
+                      <Select
+                        defaultValue={String(field.value)}
+                        onValueChange={field.onChange}
+                        {...field}
+                        value={String(field.value)}
+                      >
+                        <SelectTrigger className="w-[180px]" disabled={disableForm}>
+                          <SelectValue placeholder="Seleccionar proyecto" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {availableProject.map((project) => (
+                            <SelectItem key={project.id} value={String(project.id)}>
+                              <div className="flex items-center gap-2">
+                                <img
+                                  alt={`${project.name} logo`}
+                                  className="size-6"
+                                  src={project.logoUrl}
+                                />
+                                <span>{project.name}</span>
+                              </div>
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </form>
+        </Form>
+        <DialogFooter>
+          <Button
+            className="bg-zinc-800 text-white hover:bg-zinc-700"
+            disabled={disableForm}
+            form="relation-with-project-from-collaborator"
+            type="submit"
+            typeof="submit"
+          >
+            Relacionar
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/components/organisms/form/update-collaborator.tsx
+++ src/components/organisms/form/update-collaborator.tsx
@@ -1,7 +1,7 @@
-import type {Collaborator} from "@prisma/client";
+import type {Collaborator, Project} from "@prisma/client";
 
 import {zodResolver} from "@hookform/resolvers/zod";
-import {Save} from "lucide-react";
+import {Save, X} from "lucide-react";
 import {useForm} from "react-hook-form";
 
 import {useToast} from "../../../hooks/use-toast";
@@ -10,12 +10,20 @@ import {putCollaborator} from "../../../services/collaborator/putCollaborator";
 import {Button} from "../../ui/button";
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "../../ui/form";
 import {Input} from "../../ui/input";
+import {Card, CardHeader} from "../../ui/card";
+import {patchDeleteRelationWithCollaborator} from "../../../services/project/patchDeleteRelationWithCollaborator";
+
+import {RelationshipCollaboratorWithProject} from "./relationship-collaborator-with-project";
 
 export function UpdateCollaboratorForm({
   defaultValues,
   disableForm,
+  availableProjects,
 }: {
-  defaultValues: Collaborator;
+  defaultValues: Collaborator & {
+    project: Pick<Project, "id" | "name" | "logoUrl">[];
+  };
+  availableProjects: Pick<Project, "id" | "name" | "logoUrl">[];
   disableForm?: boolean;
 }) {
   const {toast} = useToast();
@@ -39,6 +47,29 @@ export function UpdateCollaboratorForm({
     if (!response.success) {
       toast({
         title: "Error al actualizar colaborador",
+        description: response.message,
+        className: "bg-red-500",
+      });
+    }
+  };
+
+  const onRemoveProject = async (idProject: number) => {
+    const response = await patchDeleteRelationWithCollaborator({
+      idFrom: idProject,
+      idTo: defaultValues.id,
+    });
+
+    if (response.success) {
+      toast({
+        title: "Proyecto eliminado",
+        description: response.message,
+        className: "bg-green-500",
+      });
+    }
+
+    if (!response.success) {
+      toast({
+        title: "Error al eliminar proyecto",
         description: response.message,
         className: "bg-red-500",
       });
@@ -108,6 +139,44 @@ export function UpdateCollaboratorForm({
           <span className="text-lg">Guardar</span>
         </Button>
       </form>
+      <div className="mt-10">
+        <h2 className="text-3xl font-medium">Editar relaciones</h2>
+        <div className="mx-5 mt-5">
+          <h3 className="text-3xl font-medium">Proyectos</h3>
+          <ul className="mt-4 flex flex-wrap gap-4">
+            {defaultValues.project.map((project) => (
+              <li key={project.id}>
+                <Card className="my-5 flex flex-col items-center justify-center rounded-lg bg-zinc-300 shadow dark:bg-zinc-800">
+                  <CardHeader className="relative flex items-center gap-2">
+                    <Button
+                      aria-label="eliminar relacion"
+                      className="absolute -right-2.5 -top-2.5 m-0 flex aspect-square size-8 items-center justify-center rounded-full bg-red-500 p-0 text-center text-white hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-500"
+                      disabled={disableForm}
+                      onClick={() => onRemoveProject(project.id)}
+                    >
+                      <X />
+                      <span className="sr-only">eliminar relacion</span>
+                    </Button>
+                    <img
+                      alt={`${project.name} logo`}
+                      className="size-16 shrink-0"
+                      src={project.logoUrl}
+                    />
+                    <span className="text-lg capitalize">{project.name}</span>
+                  </CardHeader>
+                </Card>
+              </li>
+            ))}
+            <li>
+              <RelationshipCollaboratorWithProject
+                availableProject={availableProjects}
+                disableForm={disableForm}
+                idFrom={defaultValues.id}
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
     </Form>
   );
 }
