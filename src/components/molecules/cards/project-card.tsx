cb3fd0fe889cccbd5188525d17e05a4b436532a2
jorgepeniaranda <peniarandajorge@gmail.com>
2025-01-02 12:23:38 -0300

    feat: enhance dark mode support with improved styling for project cards and pages

--- src/components/molecules/cards/project-card.tsx
+++ src/components/molecules/cards/project-card.tsx
@@ -47,14 +47,14 @@ export function ProjectCard({projectKey, name, logoURL}: Props) {
         <span className="sr-only">{`${name} details`}</span>
         <img alt={name} className="size-full" src={logoURL} />
       </a>
-      <div className="flex h-1/5 items-center justify-between rounded-b-lg bg-gray-200 p-4">
-        <h3 className="font-medium tracking-tight text-gray-500">{name}</h3>{" "}
+      <div className="flex h-1/5 items-center justify-between rounded-b-lg bg-gray-200 p-4 dark:bg-zinc-800">
+        <h3 className="font-medium tracking-tight text-gray-500 dark:text-gray-400">{name}</h3>{" "}
         <button aria-label={isLiked ? "Remove from liked" : "Add to liked"} type="button">
           {/* Conditionally render filled or empty heart based on the liked state */}
           {isLiked ? (
             <HeartOff className="size-5 text-red-500" onClick={handleClickHeart} />
           ) : (
-            <Heart className="size-5 text-gray-500" onClick={handleClickHeart} />
+            <Heart className="size-5 text-gray-500 dark:text-gray-400" onClick={handleClickHeart} />
           )}
         </button>
       </div>
