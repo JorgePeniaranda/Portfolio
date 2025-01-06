da5a8ab1cbfb523ab79f8522b63003a69929bb5e
jorgepeniaranda <peniarandajorge@gmail.com>
2025-01-06 18:26:49 -0300

    feat: update delete functions to use DeleteResponse type for improved response handling

--- src/services/project/deleteProject.ts
+++ src/services/project/deleteProject.ts
@@ -1,10 +1,13 @@
-import type {ApiResponse} from "@/types/responses";
+import type {ApiResponse, DeleteResponse} from "@/types/responses";
 import type {Project} from "@prisma/client";
 
 import axios from "axios";
 
-export async function deleteProject(data: Project["id"][]): Promise<ApiResponse<Project>> {
-  const {data: response} = await axios.post("/api/project/delete", data);
+export async function deleteProject(data: Project["id"][]): Promise<ApiResponse<DeleteResponse>> {
+  const {data: response} = await axios.post<ApiResponse<DeleteResponse>>(
+    "/api/project/delete",
+    data,
+  );
 
   return response;
 }
