bacb324072665f30033d91d4a216d7a324f2a4ed
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-01-07 18:16:15 -0300

    feat: implement API endpoint to fetch paginated list of projects and create service for minimal project data retrieval

---
+++ src/pages/api/project/get/all.ts
@@ -0,0 +1,29 @@
+import type {APIRoute} from "astro";
+
+import {databaseClient} from "@/helpers/client/prisma";
+import {BuildPaginationByURL} from "@/helpers/common/build-pagination";
+import {RequestHandler} from "@/helpers/common/request-handler";
+import {fromPaginationRequestToPrismaPagination} from "@/mappers/common/fromPaginationRequestToPrismaPagination";
+
+/**
+ * GET handler to fetch a paginated list of projects.
+ * - Pagination is optional. If provided, it must be a positive numeric value greater than 0.
+ */
+export const GET: APIRoute = ({request}) => {
+  return RequestHandler(
+    async () => {
+      const paginationParams = BuildPaginationByURL(request.url);
+
+      const response = await databaseClient.project.findMany({
+        ...fromPaginationRequestToPrismaPagination(paginationParams),
+      });
+
+      return {
+        success: true,
+        message: "Projects fetched successfully",
+        data: response,
+      };
+    },
+    {successStatusCode: 200},
+  );
+};
---
+++ src/services/project/getAllProjectMin.ts
@@ -0,0 +1,25 @@
+import type {ApiResponse, PaginationRequest} from "@/types/responses";
+import type {Project} from "@prisma/client";
+
+import {apiClient} from "@/helpers/client/axios";
+
+export type IGetAllProjectMinResponse = Array<
+  Pick<Project, "id" | "key" | "name" | "logoUrl" | "stackCategory" | "status">
+>;
+
+export async function getAllProjectMin(
+  pagination?: PaginationRequest,
+): Promise<IGetAllProjectMinResponse> {
+  const {data: response} = await apiClient.get<ApiResponse<IGetAllProjectMinResponse>>(
+    "/api/project/get/min/all",
+    {
+      params: pagination,
+    },
+  );
+
+  if (response.success === false) {
+    throw new Error(response.message);
+  }
+
+  return response?.data ?? [];
+}
