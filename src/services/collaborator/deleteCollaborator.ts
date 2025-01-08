ea615f17e872dbffe64d31e3553847f4099c52e9
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-01-08 11:26:03 -0300

    fix: update API endpoint for deleting collaborators to correct path

--- src/services/collaborator/deleteCollaborator.ts
+++ src/services/collaborator/deleteCollaborator.ts
@@ -10,7 +10,7 @@ export async function deleteCollaborator(
 ): Promise<ApiResponse<DeleteResponse>> {
   try {
     const {data: response} = await axios.post<ApiResponse<DeleteResponse>>(
-      "/api/collaborator/deleted",
+      "/api/collaborator/delete",
       data,
     );
 
