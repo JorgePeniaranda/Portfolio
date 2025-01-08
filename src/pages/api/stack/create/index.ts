5b2a6431a45e97dd1839d2b7354c97d21a52d970
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-01-07 21:24:34 -0300

    refactor: enhance error handling and success responses in project and stack creation APIs

--- src/pages/api/stack/create/index.ts
+++ src/pages/api/stack/create/index.ts
@@ -1,5 +1,7 @@
 import type {APIRoute} from "astro";
 
+import {Prisma} from "@prisma/client";
+
 import {databaseClient} from "@/helpers/client/prisma";
 import {RequestHandler} from "@/helpers/common/request-handler";
 import {StackCreateSchema} from "@/schemas/stack/create";
@@ -13,17 +15,30 @@ import {StackCreateSchema} from "@/schemas/stack/create";
 export const POST: APIRoute = ({request}) => {
   return RequestHandler(
     async () => {
-      const body = await request.json();
-      const validationResult = StackCreateSchema.parse(body);
-      const response = await databaseClient.stack.create({
-        data: validationResult,
-      });
+      try {
+        const body = await request.json();
+        const validationResult = StackCreateSchema.parse(body);
+        const response = await databaseClient.stack.create({
+          data: validationResult,
+        });
 
-      return {
-        success: true,
-        message: "Stack created successfully",
-        data: response,
-      };
+        return {
+          success: true,
+          message: "Stack created successfully",
+          data: response,
+        };
+      } catch (error) {
+        if (
+          error instanceof Prisma.PrismaClientKnownRequestError &&
+          error.code === "P2002" &&
+          Array.isArray(error.meta?.target) &&
+          error.meta?.target.includes("key")
+        ) {
+          throw new Error("The provided key is already in use");
+        }
+
+        throw error;
+      }
     },
     {successStatusCode: 201},
   );
