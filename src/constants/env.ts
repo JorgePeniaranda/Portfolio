896dc8135fbf870cb2a05522b9271552e8e2410f
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-02-12 22:13:58 -0300

    refactor: simplify environment variable handling in env.ts for clarity

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,21 +1,15 @@
 import { getEnvValue } from '@/helpers/common/get-env';
 import packageJson from '@/../package.json';
 
-// Check if the environment is production based on the `import.meta.env.PROD` value
-const isProduction = import.meta.env.PROD;
-
-// Check if the environment is development based on the `import.meta.env.DEV` value
-const isDevelopment = import.meta.env.DEV;
-
 /**
  * Configuration object for environment-specific values.
  * @constant {object}
  */
 export const ENV = {
-  isProduction,
-  isDevelopment,
-  base_url: isProduction ? getEnvValue('SITE') : 'http://localhost:4321',
-  secret_code: getEnvValue('PUBLIC_SECRET_CODE', '7421'),
   appVersion: packageJson.version,
-  isServerSideEnable: isDevelopment,
+  isProduction: import.meta.env.PROD,
+  isDevelopment: import.meta.env.DEV,
+  isServerSideEnable: import.meta.env.SSR,
+  base_url: 'http://localhost:4321', // import.meta.env.PROD ? import.meta.env.SITE : 'http://localhost:4321',
+  secret_code: getEnvValue('PUBLIC_SECRET_CODE', '7421'),
 } as const;
