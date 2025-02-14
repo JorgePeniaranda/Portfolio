f2dee475862a988df9eec1828e557f09d128a898
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-02-14 09:59:09 -0300

    refactor: replace static Axios mocks with dynamic generation for improved test accuracy and consistency

--- __test__/services/collaborator/postCollaborator.test.ts
+++ __test__/services/collaborator/postCollaborator.test.ts
@@ -1,9 +1,8 @@
-import type { ErrorResponse } from '@/types/responses';
 import type { Collaborator } from '@prisma/client';
-import type { AxiosError } from 'axios';
 
-import { AxiosHeaders, type AxiosResponse } from 'axios';
-import { describe, expect, it, vi } from 'vitest';
+import { createMockAxiosError } from '__test__/__mock__/create-mock-axios-error';
+import { createMockAxiosResponse } from '__test__/__mock__/create-mock-axios-response';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { generateTestCollaboratorMock } from '../../__mock__/collaborator.mock';
 
@@ -14,66 +13,62 @@ import { postCollaborator } from '@/services/collaborator/postCollaborator';
 vi.mock('@/helpers/client/axios');
 
 describe('postCollaborator', () => {
-  const input = generateTestCollaboratorMock();
-  const APIUrl = '/api/collaborator';
+  /**
+   * Mocked request body for the service.
+   */
+  const MockCollaboratorRequest = generateTestCollaboratorMock();
+
+  /**
+   * Mocked response body for axios when the request is successful.
+   */
+  const MockAxiosResponse = createMockAxiosResponse<Collaborator>({
+    data: generateTestCollaboratorMock(),
+  });
+
+  /**
+   * Mocked error response for axios when the request fails.
+   */
+  const MockAxiosError = createMockAxiosError({
+    response: {
+      data: {
+        title: 'An test error occurred',
+      },
+    },
+  });
+
+  /**
+   * API endpoint for the service.
+   */
+  const EndpointUrl = '/api/collaborator';
+
+  beforeEach(() => {
+    vi.restoreAllMocks();
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  afterEach(() => {
+    expect(apiClient.post).toHaveBeenCalledWith(EndpointUrl, MockCollaboratorRequest);
+  });
 
   it('should return a successful response when the request is correct', async () => {
-    // Mock a successful response
-    const mockResponse: AxiosResponse<Collaborator> = {
-      config: {
-        headers: new AxiosHeaders(),
-      },
-      headers: {},
-      status: 201,
-      statusText: 'OK',
-      data: input,
-    };
+    vi.mocked(apiClient.post).mockResolvedValueOnce(MockAxiosResponse);
 
-    // Simulate a resolved promise for apiClient.post
-    vi.mocked(apiClient.post).mockResolvedValueOnce(mockResponse);
-    const response = await postCollaborator(input);
+    const response = await postCollaborator(MockCollaboratorRequest);
 
-    // Validate response and apiClient call
-    expect(response).toEqual(mockResponse.data);
-    expect(apiClient.post).toHaveBeenCalledWith(APIUrl, input);
+    expect(response).toEqual(MockAxiosResponse.data);
   });
 
   it('should handle errors correctly when the request fails', async () => {
-    // Mock an error response (axios error)
-    const mockError: AxiosError<ErrorResponse> = {
-      isAxiosError: true,
-      message: 'Request failed with status code 500',
-      name: 'AxiosError',
-      toJSON: () => ({}),
-      response: {
-        config: {
-          headers: new AxiosHeaders(),
-        },
-        headers: {},
-        status: 500,
-        statusText: 'Internal Server Error',
-        data: {
-          status: 500,
-          title: 'An internal server error occurred.',
-          type: 'InternalServerError',
-          detail: 'This is an test error message',
-        },
-      },
-    };
-
-    // Simulate a rejected promise for apiClient.post
-    vi.mocked(apiClient.post).mockRejectedValueOnce(mockError);
+    vi.mocked(apiClient.post).mockRejectedValueOnce(MockAxiosError);
 
     try {
-      await postCollaborator(input);
+      await postCollaborator(MockCollaboratorRequest);
     } catch (error) {
-      // Validate error handling and apiClient call
       expect(error).toBeInstanceOf(Error);
       if (error instanceof Error) {
-        expect(error.message).toBe(mockError.response?.data.title);
+        expect(error.message).toBe(MockAxiosError.response?.data.title);
       }
     }
-
-    expect(apiClient.post).toHaveBeenCalledWith(APIUrl, input);
   });
 });
