9af71373827a5878c6f949207737540486f94edd
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-02-19 20:16:34 -0300

    refactor: update environment configuration to use destructured import and include additional properties

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,16 +1,20 @@
 import { getEnvValue } from '@/helpers/common/get-env';
-import packageJson from '@/../package.json';
+import { version } from '@/../package.json';
+
+const { DEV, PROD, SITE, SSR, BASE_URL, ASSETS_PREFIX, MODE } = import.meta.env;
 
 /**
  * Configuration object for environment-specific values.
  */
 export const ENV = {
-  appVersion: packageJson.version,
-  isProduction: import.meta.env.PROD,
-  isDevelopment: import.meta.env.DEV,
-  isServerSideEnable: import.meta.env.SSR,
-  site_url: import.meta.env.SITE,
-  // base_url: import.meta.env.BASE_URL,
+  appVersion: version,
+  isProduction: PROD,
+  isDevelopment: DEV,
+  isServerSideEnable: SSR,
+  site_url: SITE,
+  base_url: BASE_URL,
+  assets_prefix: ASSETS_PREFIX,
+  mode: MODE,
   api_url: 'http://localhost:4321',
   secret_code: getEnvValue('PUBLIC_SECRET_CODE', '7421'),
 } as const;
