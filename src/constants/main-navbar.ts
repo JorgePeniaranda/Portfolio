f1e2c4613fd28ed7c4dad5db514b8ad3bbdbe4c3
Jorge Peñaranda <80211041+JorgePeniaranda@users.noreply.github.com>
2025-02-20 10:22:03 -0300

    refactor: remove unnecessary comments and enforce const assertions in constants

--- src/constants/main-navbar.ts
+++ src/constants/main-navbar.ts
@@ -76,4 +76,4 @@ export const NAVBAR_ITEMS: NavbarSection[] = [
       },
     ],
   },
-];
+] as const;
--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,6 +1,5 @@
 import type { Prisma } from '@prisma/client';
 
-// Type representing the possible values of themes ('light' or 'dark')
 export const themes = {
   light: 'light',
   dark: 'dark',
@@ -10,7 +9,6 @@ export type theme = (typeof themes)[keyof typeof themes];
 
 export const themesArray = Object.values(themes);
 
-// Type representing a boolean value for the sound state
 export type ISoundState = boolean;
 
 export type PrismaError =
